bb5cc5aa737346933ea0d6a9fb383d5f44a66871
Sergey Strukov <sshimnick@hotmail.com>
2023-11-23 13:25:18 +0300

    update

---
+++ src/MakeLDopt/main.cpp
@@ -0,0 +1,59 @@
+/* main.cpp */
+//----------------------------------------------------------------------------------------
+//
+//  Project: LightForge 1.00
+//
+//  License: Boost Software License - Version 1.0 - August 17th, 2003
+//
+//            see http://www.boost.org/LICENSE_1_0.txt or the local copy
+//
+//  Copyright (c) 2023 Sergey Strukov. All rights reserved.
+//
+//----------------------------------------------------------------------------------------
+
+#include <exception>
+
+#include "FullBaseList.h"
+
+namespace App {
+
+/* Main() */
+
+int Main()
+ {
+  std::cout << "LightForge MakeLDopt 1.00" << std::endl ;
+  std::cout << "Copyright (c) 2023 Sergey Strukov. All rights reserved." << std::endl << std::endl ;
+
+  FullBaseList bases;
+
+  bases.buildLDopt();
+
+  return 0;
+ }
+
+} // namespace App
+
+/* main() */
+
+int main()
+ {
+  try
+    {
+     return App::Main();
+    }
+  catch(std::exception &ex)
+    {
+     std::cout << std::endl << "Exception: " << ex.what() << std::endl ;
+
+     return 1;
+    }
+  catch(...)
+    {
+     std::cout << std::endl << "Unknown exception" << std::endl ;
+
+     return 1;
+    }
+ }
+
+
+
